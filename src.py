7797840a1fabb815611e6c4dfa9bcce8b2aecd48
redowan <redowan.nafi@gmail.com>
2022-01-05 05:45:16 +0600

    Notes on Python's string interning, closes #20

--- src.py
+++ src.py
@@ -1,45 +1,69 @@
-# Placeholder python file to test the snippets
-from __future__ import annotations
-
-from typing import Protocol, runtime_checkable
+# import time
+# from functools import lru_cache
 
 
-@runtime_checkable
-class ProtoFoo(Protocol):
-    def bar(self) -> str:
-        ...
-
-    @classmethod
-    def baz(cls) -> str:
-        ...
-
-    @property
-    def qux(self) -> str:
-        ...
+# @lru_cache
+# def slow_add(*args, delay=1):
+#     time.sleep(delay)
+#     return sum(args)
 
 
-class Foo:
-    def bar(self) -> str:
-        return "from instance method"
+# s1 = time.perf_counter()
 
-    @classmethod
-    def baz(cls) -> str:
-        return "from class method"
+# for i in range(5):
+#     print(f"Result of run {i} is: {slow_add(1,2,3)}")
 
-    @property
-    def qux(self) -> str:
-        return "from property method"
+# s2 = time.perf_counter()
+# print(f"Total runtime: {s2-s1} seconds.")
 
 
-def run(foo: ProtoFoo) -> None:
-    if not isinstance(foo, ProtoFoo):
-        raise Exception("Foo do not conform to Protofoo interface")
+# import timeit
 
-    print(foo.bar())
-    print(foo.baz())
-    print(foo.qux)
+# # Implicitly interned dict creation and access.
+# implicitly_interned = """
+# d = {"#"*4096 : "Interned"}
+# d["#"*4096]
+# """
+
+# # Explicitly interned dict creation and access.
+# explicitly_interned = """
+# k = sys.intern("#"*4097)
+# d = { k : "Explicitly-interned"}
+# d[k]
+# """
+
+# # print(f"Interned dict creation & access: {timeit.timeit(interned)} seconds")
+# # print(f"Non-interned dict creation & access: {timeit.timeit(non_interned)} seconds")
+
+# print(timeit.timeit(explicitly_interned)/timeit.timeit(implicitly_interned))
+
+import sys
+import time
+
+# Implicitly interned.
+t0 = time.perf_counter()
+
+for _ in range(10000):
+    d = {"#"*4096 : "Interned"}
+    d["#"*4096]
+
+t1 = time.perf_counter()
 
 
-if __name__ == "__main__":
-    foo = Foo()
-    run(foo)
+# Explicitly interned.
+t2 = time.perf_counter()
+
+k1 = sys.intern("#"*4097)
+k2 = sys.intern("#"*4097)
+for _ in range(10000):
+    d = {k1 : "Interned"}
+    d[k2]
+
+t3 = time.perf_counter()
+
+
+print(t1-t0)
+print((t3-t2)/ (t1-t0))
+print(f"Implicitly interned dict creation & access: {t1-t0} seconds")
+print(f"Explicitly interned dict creation & access: {t3-t2} seconds")
+print(f"Explicitly interned creation & access is {(t3-t2)/(t1-t0)} times slower")
