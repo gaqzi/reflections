20ac41d326b367befd0d12a374fc21da2591da4a
redowan <redowan.nafi@gmail.com>
2022-02-06 06:05:56 +0600

    Publish amphibian decorators blog, closes #54

--- src.py
+++ src.py
@@ -1,103 +1,116 @@
-from __future__ import annotations
-
-from typing import Generic, TypeVar, Union
-
-T = TypeVar("T")
-E = TypeVar("E", bound=Exception)
-
-
-class Ok(Generic[T]):
-    def __init__(self, value: T) -> None:
-        self._value = value
-
-    def ok(self) -> T:
-        return self._value
-
-
-class Err(Generic[E]):
-    def __init__(self, e: E) -> None:
-        self._e = e
-
-    def err(self) -> E:
-        return self._e
-
-
-Result = Union[Ok[T], Err[E]]
-
-
-def div(dividend: int, divisor: int) -> Result[int, ZeroDivisionError]:
-    if divisor == 0:
-        return Err(ZeroDivisionError("Zero division error occured!"))
-
-    return Ok(dividend // divisor)
-
-
-Convertible = TypeVar("Convertible", int, float, str)
-IntResult = Result[int, TypeError]
-
-
-def to_int(num: Convertible) -> IntResult:
-    if not isinstance(num, (int, float, str)):
-        return Err(TypeError("Input type is not convertible to integer."))
-
-    return Ok(int(num))
-
-
-if __name__ == "__main__":
-    result = to_int(1)
-
-    if isinstance(result, Ok):
-        print(result.ok())
-    else:
-        print(result.err())
-
-
 # from __future__ import annotations
 
-# # In < Python 3.9, import this from the 'typing' module.
-# from collections.abc import Generator
-# from pathlib import Path
-# from typing import Any
+# import asyncio
+# import logging
+# from collections.abc import AsyncIterator, Awaitable
 
-# d = {
-#     k: v
-#     for k, v in zip(
-#         "smart", ["a", {"x": "b"}, "c", {"y": ["hello", "world"]}, "d", "e"]
-#     )
-# }
+# import httpx
 
+# logging.basicConfig(level=logging.INFO)
 
-# def find_first(d: dict, key: str, default: str | None = None) -> str | None:
-#     """Find the first value against a key in an arbitrarily nested
-#     dictionary.
-#     """
-#     stack = [d.items()]
+# TIMEOUT: float = 10_000  # ms
 
-#     while stack:
-#         for k, v in stack[-1]:
-#             if isinstance(v, dict):
-#                 stack.append(v.items())
-#                 break
-#             elif k == key:
-#                 return v
-#         else:
-#             stack.pop()
-#     return default
+# # We don't want to overwhelm the server.
+# CONCURRENCY_LIMIT: int = 10
 
 
-# def list_files(root: str = ".") -> Generator[Any, None, None] | None:
-#     """Recursively find files in a directory."""
+# async def collect_test_uuid(order_code: str) -> AsyncIterator[str]:
+#     url = f"http://southernlabpartners.local.dendi-dev.com:8000/api/v2/orders/test_results/?order_code={order_code}"
 
-#     root = Path(root)
+#     headers = {
+#         "Authorization": "Token ec2631da8b3c76300b0c0c8840a7c608a6ee8172",
+#         "Content-Type": "application/json",
+#     }
 
-#     stack = [root.iterdir()]
-#     while stack:
-#         for path in stack[-1]:
-#             if path.is_dir():
-#                 stack.append(path.iterdir())
-#                 break
+#     async with httpx.AsyncClient(headers=headers) as client:
+#         r = await client.get(url)
+#         tests = r.json()["results"].pop()["tests"]
 
-#             elif path.is_file():
-#                 yield str(path).split("/")[-1]
-#         else:
-#             stack.pop()
+#         for i, test in enumerate(tests):
+#             if i ==0 or i == 1:
+#                 continue
+#             yield test["uuid"]
+
+
+# async def make_request(uuid: str) -> Awaitable[None]:
+#     url = f"http://southernlabpartners.local.dendi-dev.com:8000/api/v2/test_results/{uuid}/"
+
+#     payload = {
+#         "result": {
+#             "submitted_datetime": "2021-11-04T12:48:52.235490",
+#             "result_quantitative": 22,
+#             "result_qualitative": "",
+#             "result_is_expected": False,
+#             "result_text": "",
+#             "result_flag": "example result flag",
+#             "days_incubated": None,
+#             "reflex_triggered": False,
+#             "unit": "mg",
+#             "reference_range_string": "example reference range string updated",
+#         }
+#     }
+#     headers = {
+#         "Authorization": "Token ec2631da8b3c76300b0c0c8840a7c608a6ee8172",
+#         "Content-Type": "application/json",
+#     }
+
+#     async with httpx.AsyncClient(headers=headers) as client:
+#         async with asyncio.Semaphore(CONCURRENCY_LIMIT):
+
+#             logging.info("Posting results against test %s" % uuid)  # no fstring.
+
+#             result = await client.put(url, json=payload, timeout=TIMEOUT)
+#             print()
+#             print("=" * 42)
+#             logging.info(result.json())
+#             print("=" * 42)
+#             print()
+
+
+# async def main(order_code: str) -> Awaitable[None]:
+#     logging.info("Collecting the test uuids from 'GET v2/orders/test_results' API")
+
+#     tasks = []
+#     async for uuid in collect_test_uuid(order_code):
+#         task = asyncio.create_task(make_request(uuid))
+#         tasks.append(task)
+
+#     await asyncio.gather(*tasks)
+
+
+# if __name__ == "__main__":
+#     asyncio.run(main(order_code="SO22-0002746"))
+# src.py
+from __future__ import annotations
+
+# import asyncio
+import inspect
+
+# In <Python 3.9, import these from the 'typing' module.
+from collections.abc import Awaitable, Callable
+from functools import wraps
+from typing import Any
+
+
+def tag(*names: str) -> Callable:
+    def decorator(func: Callable) -> Callable:
+        # Tagging has to happen in function definition time.
+        # Othewise calling func._tags will raise AttributeError.
+        func._tags = names  # type: ignore
+
+        if inspect.iscoroutinefunction(func):
+
+            @wraps(func)
+            async def async_wrapped(*args: Any, **kwargs: Any) -> Awaitable[Any]:
+                return await func(*args, **kwargs)
+
+            return async_wrapped
+        else:
+
+            @wraps(func)
+            def sync_wrapped(*args: Any, **kwargs: Any) -> Any:
+                return func(*args, **kwargs)
+
+            return sync_wrapped
+
+    return decorator
